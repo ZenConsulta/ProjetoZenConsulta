1718aca92226b080f2b6dcc834d3c52218eed44e
Caio Cardoso Terra <caio.terra@gsuite.iff.edu.br>
2026-06-22 21:22:16 -0300

    ADD ARchives Spring Boot
    Adicionando arquivos de acordo com a aula

---
+++ docs/arquitetura/ZenConsulta/src/test/java/br/edu/iff/ccc/ZenConsulta/ZenConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.ZenConsulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
