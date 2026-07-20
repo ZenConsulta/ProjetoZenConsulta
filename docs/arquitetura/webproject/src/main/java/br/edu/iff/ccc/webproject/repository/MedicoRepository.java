c3385998b99c6d63e6f61b8f1ee4a4f71283716c
Patryck Rangel Rocha Chaves <patryck.chaves@gsuite.iff.edu.br>
2026-07-20 17:50:20 -0300

    Create MedicoRepository interface for Medico operations

---
+++ docs/arquitetura/webproject/src/main/java/br/edu/iff/ccc/webproject/repository/MedicoRepository.java
@@ -0,0 +1,11 @@
+package br.edu.iff.ccc.webproject.repository;
+
+import br.edu.iff.ccc.webproject.model.Medico;
+import java.util.List;
+import java.util.Optional;
+
+public interface MedicoRepository {
+    Medico salvar(Medico medico);
+    List<Medico> listarTodos();
+    Optional<Medico> buscarPorId(String id);
+}
